7bf5ec5db1b037678f76a85ccbe59214763ba230
Mobashir Monim <mobashirmonim@gmail.com>
2021-12-06 09:19:22 +0600

    Put phone col after password

--- database/migrations/2021_12_04_083513_add_phone_column_to_users_table.php
+++ database/migrations/2021_12_04_083513_add_phone_column_to_users_table.php
@@ -14,7 +14,7 @@ class AddPhoneColumnToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone')->nullable();
+            $table->string('phone')->nullable()->after('password');
         });
     }
 
