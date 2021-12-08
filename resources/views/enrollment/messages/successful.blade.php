4b1aa0d0639c63a2a2b6354e53061c0c03be30a1
Mobashir Monim <mobashirmonim@gmail.com>
2021-12-08 14:59:54 +0600

    Create views for payment gateway messages

---
+++ resources/views/enrollment/messages/successful.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row justify-content-center min-h">
+        <div class="col-md-8 my-auto">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row mt-5 mb-4">
+                        <div class="col-md-12 text-center">
+                            <h4 class="mb-2">Could not complete enrollment in <strong>{{ $course->name }}</strong>!!</h4>
+                            <h5 class="mb-3">Please check your email to setup login credentials on BuSSO</h5>
+                            <span class="material-icons-round d-none d-md-inline-block" style="font-size: 8rem">sentiment_very_satisfied</span>
+                            <span class="material-icons-round d-none d-md-inline-block" style="font-size: 8rem">sentiment_very_satisfied</span>
+                            <span class="material-icons-round d-none d-md-inline-block" style="font-size: 8rem">sentiment_very_satisfied</span>
+
+                            <span class="material-icons-round d-md-none" style="font-size: 4rem">sentiment_very_satisfied</span>
+                            <span class="material-icons-round d-md-none" style="font-size: 4rem">sentiment_very_satisfied</span>
+                            <span class="material-icons-round d-md-none" style="font-size: 4rem">sentiment_very_satisfied</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
