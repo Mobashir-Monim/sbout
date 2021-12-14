1c6edd672bd8b939906c3e0ccd2fec5f7d3b5539
Mobashir Monim <mobashirmonim@gmail.com>
2021-12-14 14:19:54 +0600

    Complete payment gateway auth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        // Payment Gateway middlewares
+        'payment-gateway.auth' => \App\Http\Middleware\PaymentGateway\Authenticator::class,
     ];
 }
--- app/Http/Middleware/PaymentGateway/Authenticator.php
+++ app/Http/Middleware/PaymentGateway/Authenticator.php
@@ -20,6 +20,15 @@ class Authenticator
     {
         $course = Course::find($request->route('course'));
         $purchase = CoursePurchase::find($request->route('purchase'));
-        return $next($request);
+
+        $flag = $request->value_a == hash('sha256', md5(config('payment-gateway.store.secret')) . md5($this->purchase->id));
+        $flag = $flag && $request->value_b == hash('sha256', md5(config('payment-gateway.store.secret')) . md5($this->purchase->created_at));
+        $flag = $flag && $request->value_c == hash('sha256', md5(config('payment-gateway.store.secret')) . md5($this->purchase->txid));
+        
+        if ($flag) {
+            return $next($request);
+        }
+
+        return abort(403, 'Unauthorized');
     }
 }
